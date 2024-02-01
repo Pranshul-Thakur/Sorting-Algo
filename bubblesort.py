40da50a27d1ba7eab1ba49b79073cdbdf590e10e
Pranshul-Thakur <118863617+Pranshul-Thakur@users.noreply.github.com>
2024-02-01 20:48:43 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,4 +1,4 @@
-'''def bubble_sort(arr):
+def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
@@ -9,7 +9,4 @@
 my_list = [3,5,7,9,1,9,0,3,2]
 bubble_sort(my_list)
 print("The sorted list is : ", my_list)
-'''
-
-def bubble_sort(arr):
-    
+   
