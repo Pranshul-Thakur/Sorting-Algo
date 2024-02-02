0f923a38a2c2886e91b0980d1decb927acaec930
Pranshul-Thakur <118863617+Pranshul-Thakur@users.noreply.github.com>
2024-02-02 14:45:19 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,12 +1,20 @@
 def bubble_sort(arr):
     n = len(arr)
+    
+    #traverse through the list
     for i in range(n):
+
+        #traversing through the list to find the next elements
         for j in range(0, n-i-1):
+
+            #checking if the current element bigger than the previous element
             if arr[j]>arr[j+1]:
+
+                #switch the 2 elements
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-                
-                
+
+
+#printing the list                
 my_list = [3,5,7,9,1,9,0,3,2]
 bubble_sort(my_list)
 print("The sorted list is : ", my_list)
-   
