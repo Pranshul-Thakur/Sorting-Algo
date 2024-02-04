de7d7ff470e2259e9dc9cff667d71df7631c47c4
Pranshul-Thakur <118863617+Pranshul-Thakur@users.noreply.github.com>
2024-02-04 13:02:02 +0530

    Add files via upload

---
+++ Mergesort.py
@@ -0,0 +1,35 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Find the middle of the array
+        left_half = arr[:mid]  # Divide the array into two halves
+        right_half = arr[mid:]
+
+        merge_sort(left_half)  # Recursive call on the left half
+        merge_sort(right_half)  # Recursive call on the right half
+
+        # Merge the two sorted halves
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Check for any remaining elements in both halves
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Example usage:
+my_array = [14, 7, 12, 3]
+merge_sort(my_array)
+print("Sorted array:", my_array)
