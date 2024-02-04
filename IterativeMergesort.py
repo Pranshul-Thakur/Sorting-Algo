80d5e1378692168061f4b40ca93f993a3c6102d1
Pranshul-Thakur <118863617+Pranshul-Thakur@users.noreply.github.com>
2024-02-04 19:00:14 +0530

    Update IterativeMergesort.py

--- IterativeMergesort.py
+++ IterativeMergesort.py
@@ -1,32 +1,35 @@
 def IterativeMergeSortAlgorithm(arr):
-    # Initialize the array size
     n = len(arr)
 
-    # Set the step size for the merging process
+    #represents the size of the sub-array to be merged during iteration
     step = 1
 
-    # Continue merging until step size is less than the array size
+    #Continue merging until step size is less than the array size
     while step < n:
-        # Iterate over the array in steps
+        
+        
+        #Iterate over the array in steps
         for left in range(0, n - step, 2 * step):
             mid = left + step - 1
             right = min(left + 2 * step - 1, n - 1)
 
-            # Merge the subarrays
-            merge(arr, left, mid, right)
+            # Merge the sub-arrays
+            merge(arr, left, mid, right) 
 
-        # Double the step size for the next iteration
+        #it doubles the size of the sub-arrays to be merged in the next iteration of the outer loop
         step *= 2
     
     return arr
 
+#Merging the 4 sorted arrays
 def merge(arr, left, mid, right):
     left_half = arr[left:mid + 1]
     right_half = arr[mid + 1:right + 1]
 
     i = j = 0
     k = left
-
+    
+    #Merging the two sorted halves
     while i < len(left_half) and j < len(right_half):
         if left_half[i] <= right_half[j]:
             arr[k] = left_half[i]
@@ -36,6 +39,7 @@ def merge(arr, left, mid, right):
             j += 1
         k += 1
 
+    #checking for any remaining elements in both halves
     while i < len(left_half):
         arr[k] = left_half[i]
         i += 1
@@ -46,7 +50,7 @@ def merge(arr, left, mid, right):
         j += 1
         k += 1
 
-# Example usage:
+#print the list
 my_array = [14, 7, 12, 3]
 sorted_array = IterativeMergeSortAlgorithm(my_array)
 print("Sorted array:", sorted_array)
