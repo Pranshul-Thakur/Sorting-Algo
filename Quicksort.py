ae14f6ebc6fe347e88bce1808d7852d1076e2602
Pranshul Thakur <118863617+Pranshul-Thakur@users.noreply.github.com>
2024-02-07 16:17:22 +0530

    Update Quicksort.py

--- Quicksort.py
+++ Quicksort.py
@@ -1,31 +1,47 @@
 def partition(array, first, last):
  
-    # Choose the rightmost element as pivot
+    #choosing the last element as pivot
     pivot = array[last]
  
-    # Pointer for greater element
+    #pointer for the larger element
     i = first - 1
  
-    # Traverse through all elements
-    # compare each element with pivot
+    #traversing through all the elements and comparing them with the pivot
     for j in range(first, last):
         if array[j] <= pivot:
  
-            # If element smaller than pivot is found
-            # swap it with the greater element pointed by i
+            #if element smaller than the pivot found, swap it with i
             i = i + 1
  
-            # Swapping element at i with element at j
+            #swapping elements at i with j
             (array[i], array[j]) = (array[j], array[i])
  
-    # Swap the pivot element with
-    # the greater element specified by i
+    #swap the pivot element with the i
     (array[i + 1], array[last]) = (array[last], array[i + 1])
  
-    # Return the position from where partition is done
+    #return the position
     return i + 1
  
  
+#performing quicksort
+def quicksort(array, first, last):
+    if first < last:
+ 
+        #finding the pivot element and checking if the smaller element are on left and larger on the right
+        pi = partition(array, first, last)
+ 
+        #recursive call on left of pivot
+        quicksort(array, first, pi - 1)
+ 
+        #recursive call on right of pivot
+        quicksort(array, pi + 1, last)
+        
+
+#printing the list
+my_list = [10,20,40,7,23,45,67]
+n = len(my_list)
+quicksort(my_list, 0, n - 1)
+print("The sorted array is : ", my_list) 
 # Function to perform quicksort
 def quicksort(array, first, last):
     if first < last:
