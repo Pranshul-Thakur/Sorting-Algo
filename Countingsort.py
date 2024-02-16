77c066e0a0acf10e395a7126a801c64d63077a48
Pranshul Thakur <118863617+Pranshul-Thakur@users.noreply.github.com>
2024-02-16 10:05:43 +0530

    Update Countingsort.py

--- Countingsort.py
+++ Countingsort.py
@@ -1,2 +1,20 @@
 def count_sort(arr):
-m = max(arr)
+    m = max(arr)
+    count = [0] * (m + 1)
+    for num in arr:
+        count[num] += 1
+        
+    for i in range(1, m + 1):
+        count[i] += count[i - 1]
+        
+    result = [0] * len(arr)
+    
+    for num in reversed(arr):
+        result[count[num] - 1] = num
+        count[num] -= 1
+        
+    return result
+
+arr = [4,3,5,6,7,82,1,4,334,67]
+sorted_arr = count_sort(arr)
+print(sorted_arr)
